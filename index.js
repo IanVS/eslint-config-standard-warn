5be51cf19af8a8761e6fa47d5a73d8347e4005e7
Ian VanSchooten <ian@badgelabsllc.com>
2015-06-28 22:22:51 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var standard = require('eslint-config-standard')
+
+for (var rule in standard.rules) {
+  if (standard.rules.hasOwnProperty(rule)) {
+    if (Array.isArray(standard.rules[rule])) {
+      if (standard.rules[rule][0] === 2) {
+        standard.rules[rule][0] = 1
+      }
+    } else {
+      if (standard.rules[rule] === 2) {
+        standard.rules[rule] = 1
+      }
+    }
+  }
+}
+
+module.exports = standard
